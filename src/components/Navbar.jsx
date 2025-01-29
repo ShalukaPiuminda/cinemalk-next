b4364412fcbc0cf06ea74550e13f80449f735bcd
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-01-29 15:17:47 +0530

    typo mistak in fetchTopRated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className='flex bg-green-400 dark:bg-gray-800 p-4 lg:text-lg justify-center gap-10'>
       <NavbarItems title="Trending" param="fetchTrending" />
-      <NavbarItems title="Top Rated" param="fetchtTopRated"/>
+      <NavbarItems title="Top Rated" param="fetchTopRated"/>
     </div>
   )
 }
