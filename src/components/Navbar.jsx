1faf14450cf3263b9b8fa2b07da38ef277079c37
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-01-31 06:48:23 +0530

    change navbar color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import NavbarItems from './NavbarItems'
 
 export default function Navbar() {
   return (
-    <div className='flex bg-green-400 dark:bg-gray-800 p-4 lg:text-lg justify-center gap-10'>
-      <NavbarItems title="Trending" param="fetchTrending" />
+    <div className='flex bg-white dark:bg-gray-800 p-4 lg:text-lg justify-center gap-10'>
+      <NavbarItems title="Trending"  param="fetchTrending" />
       <NavbarItems title="Top Rated" param="fetchTopRated"/>
     </div>
   )
